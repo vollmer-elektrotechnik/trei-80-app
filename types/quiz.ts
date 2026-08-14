5b5339d77d819684e9c2136a2b9868c9a37d124f
Julian Vollmer <julian@vollmer-elektrotechnik.de>
2026-08-14 22:38:28 +0200

    bugfix category

--- types/quiz.ts
+++ types/quiz.ts
@@ -1,4 +1,12 @@
-export type Category = 'VDE-AR-N 4100' | 'TAB' | 'NAV' | 'DIN VDE 0100-600' | 'Berechnungen';
+export type Category = 
+  | 'VDE-AR-N 4100 & TAB'
+  | 'NAV & Recht'
+  | 'DIN VDE 0100-600'
+  | 'Berechnungen'
+  // Optionale Abwärtskompatibilität, falls noch alte Fragen in der DB liegen:
+  | 'VDE-AR-N 4100'
+  | 'TAB'
+  | 'NAV';
 
 export interface Question {
   id: string;
