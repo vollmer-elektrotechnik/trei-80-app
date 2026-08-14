1f257bf3bc1c46775885de52efb10bbf3dde3da3
Julian Vollmer <julian@vollmer-elektrotechnik.de>
2026-08-14 22:44:50 +0200

    Bugfix III

--- types/quiz.ts
+++ types/quiz.ts
@@ -1,12 +1,11 @@
 export type Category = 
   | 'VDE-AR-N 4100 & TAB'
   | 'NAV & Recht'
-  | 'Schutzmaßnahmen & Netzformen'  // <-- Hier hinzufügen
+  | 'Schutzmaßnahmen & Netzformen'
+  | 'Leitungsberechnung & Verlegearten'
   | 'DIN VDE 0100-600'
   | 'Berechnungen'
-  | 'VDE-AR-N 4100'
-  | 'TAB'
-  | 'NAV';
+  | (string & {}); // Erlaubt jeden neuen String und verhindert zukünftige Build-Fehler
 
 export interface Question {
   id: string;
